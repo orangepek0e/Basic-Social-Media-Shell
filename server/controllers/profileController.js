74174a70e53c75c36acf24a6dd270bf596448c74
orangepek0e <georgejohnston13@hotmail.com>
2016-01-19 17:29:36 -0400

    Stick, stick, stick to your guns.

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -3,7 +3,7 @@ var fs = require('fs-extra');
 var path = require('path');
 
 module.exports.updatePhoto = function(req, res){
-    var file = req.body.file;
+    var file = req.files.file;
     var userId = req.body.userId;
 
     console.log("User "+ userId + " is submitting ", file);
